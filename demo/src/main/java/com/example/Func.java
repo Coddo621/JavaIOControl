6c98a087f75bd5755e0cf97be8334fe37e652828
Coddo621 <lovro.kacic1@gmail.com>
2024-05-02 00:38:29 +0200

    added listdirectory, getfileperms, filedirectoryrename, wordocunt and movefile, corrected a couple of spelling errors

--- demo/src/main/java/com/example/Func.java
+++ demo/src/main/java/com/example/Func.java
@@ -1,16 +1,19 @@
 package com.example;
 
+import java.io.BufferedReader;
 import java.io.File;
-import java.io.IOException;
-import java.io.FileWriter;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Func {
     static void createfile(){
         Scanner input = new Scanner(System.in);
+
         try{
-            System.out.println("Input file path, name and extension: ");
+            System.out.println("Input the file path, name and extension of the file you want to create: ");
             String filepath = input.nextLine();
 
             File file = new File(filepath);
@@ -24,13 +27,15 @@ public class Func {
             System.out.println("Exception IO Error");
             error.printStackTrace();
         }
+
         input.close();
     }
 
     static void writefile(){
         Scanner input = new Scanner(System.in);
+
         try{	
-            System.out.println("Input file path, name and extension of the file you want to write in: ");
+            System.out.println("Input the file path, name and extension of the file you want to write in: ");
             String filepath = input.nextLine();
             
             FileWriter file = new FileWriter(filepath);
@@ -41,13 +46,15 @@ public class Func {
             System.out.println("Exception IO Error");
             error.printStackTrace();
         }
+
         input.close();
     }
     
     static void readfile(){
         Scanner input = new Scanner(System.in);
+
         try{
-            System.out.println("Input file path, name and extension of the file you want to read: ");
+            System.out.println("Input the file path, name and extension of the file you want to read: ");
             String filepath = input.nextLine();
             
             File file = new File(filepath);
@@ -63,59 +70,173 @@ public class Func {
             System.out.println("Exception IO Error");
             error.printStackTrace();
         }
+
         input.close();
     }
     
     static void fileinfo(){
-        Scanner input = new Scanner(System.in); 
-        System.out.println("Input file path, name and extension of the file you want to inspect: ");
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Input the file path, name and extension of the file you want to inspect: ");
         String filepath = input.nextLine();
-        
         File file = new File(filepath);
+
         if(file.exists()){
             System.out.println("Name: "+filepath+"\nDirectory: "+file.getAbsolutePath()+"\nWriteable: "+file.canWrite()+"\nReadable: "+file.canRead()+"\nSize: "+file.length());
         }else{
             System.out.println("File doesn't exist");
         }
+
         input.close();
     }
     
     static void createdir(){
         Scanner input = new Scanner(System.in);
-        System.out.println("Input path of the directory you want to create: ");
+
+        System.out.println("Input the path of the directory you want to create: ");
         String dirpath = input.nextLine();
         File dir = new File(dirpath);
+
         if(dir.mkdir()){
             System.out.println("Directory created: "+dir.getName());
         }else{
             System.out.println("An error occured while creating directory");
         }
+
         input.close();
     }
     
     static void deletefile(){
         Scanner input = new Scanner(System.in);
+
         System.out.println("Input the path of the file you want to delete: ");
         String filepath = input.nextLine();
         File file = new File(filepath);
+
         if(file.delete()){
             System.out.println("File deleted: "+file.getName());
         }else{
             System.out.println("Couldn't delete file");
         }
+
         input.close();
     }
     
     static void deletedirectory(){
         Scanner input = new Scanner(System.in);
-        System.out.println("Input path of the directory you want to delete: ");
+
+        System.out.println("Input the path of the directory you want to delete: ");
         String dirpath = input.nextLine();
         File dir = new File(dirpath);
+
         if(dir.delete()){
             System.out.println("Directory deleted: "+dir.getName());
         }else{
             System.out.println("Couldn't delete directory");
         }
+
+        input.close();
+    }
+
+    static void listdirectory(){
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Input the path of the directory you want to list out: ");
+        String dirpath = input.nextLine();
+        File dir = new File(dirpath);
+        File[] filelist = dir.listFiles();
+
+        if(filelist != null){
+            for(int i = 0; i  < filelist.length; i++){
+                if(filelist[i].isFile()){
+                    System.out.println("File: " + filelist[i].getName());
+                }else if(filelist[i].isDirectory()){
+                    System.out.println("Directory: " + filelist[i].getName());
+                }
+            }
+        }else{
+            System.out.println("You haven't selected a directory to scan");
+        }
+
+        input.close();
+    }
+
+    static void getfileperms(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Input the path of the file you want to get the permissions of: ");
+        String filepath = input.nextLine();
+        File file = new File(filepath);
+
+        if(file.exists()){
+            System.out.println("Write: " + file.canWrite());
+            System.out.println("Read: " + file.canRead());
+            System.out.println("Execute: " + file.canExecute());
+        }else{
+            System.out.println("File not found");
+        }
+
+        input.close();
+    }
+
+    static void filedirectoryrename(){
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Input the path of the file or directory you want to rename: ");
+        String filepath = input.nextLine();
+        System.out.println("Input the name you want to rename the file or directory into: ");
+        String renamenewname = input.nextLine();
+        File file = new File(filepath);
+        String renamestring = file.getParent() + "/" + renamenewname;
+        File rename = new File(renamestring);
+        boolean renameflag = file.renameTo(rename);
+
+        if(renameflag == true){
+            System.out.println("File renamed to: " + rename.getName());
+        }else{
+            System.out.println("You haven't selected the original and new file or directory name");
+        }
+
+        input.close();
+    }
+
+    static void wordcount() throws IOException{
+        String line;
+        int count = 0;
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Input the path of the file you want to count the words in: ");
+        String filepath = input.nextLine();
+        FileReader file = new FileReader(filepath);
+        BufferedReader reader = new BufferedReader(file);
+
+        while((line = reader.readLine()) != null){
+            String words[] = line.split(" ");
+            count = count + words.length;
+        }
+
+        System.out.println("Number of words in file: "+count);
+
+        input.close();
+        reader.close();
+    }
+
+    static void movefile(){
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Input the path of the file you want to move: ");
+        String filepath = input.nextLine();
+        System.out.println("Input the path you want to move the file to:  ");
+        String movepath = input.nextLine();
+        File file = new File(filepath);
+        File move = new File(movepath);
+        boolean moveflag = file.renameTo(move);
+
+        if(moveflag == true){
+            System.out.println("File moved to: " + move.getAbsolutePath());
+        }else{
+            System.out.println("You haven't selected the path you want to move the file to");
+        }
+
         input.close();
     }
 }
--- demo/src/main/java/com/example/Main.java
+++ demo/src/main/java/com/example/Main.java
@@ -1,10 +1,11 @@
 package com.example;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner input = new Scanner(System.in);
-		System.out.println("1 Create file\n2 Write to file\n3 Read file\n4 File info\n5 Create directory\n6 Delete file\n7 Delete directory");
+		System.out.println("1 Create file\n2 Write to file\n3 Read file\n4 File info\n5 Create directory\n6 Delete file\n7 Delete directory\n8 List files and directories at given path\n9 Get file permissions\n10 Rename file or directory\n11 Count words in file\n12 Move file");
         int choice = input.nextInt();
         switch(choice) {
 			case 1:
@@ -28,9 +29,25 @@ public class Main {
 			case 7:
 				Func.deletedirectory();
 				break;
+			case 8:
+				Func.listdirectory();
+				break;
+			case 9:
+				Func.getfileperms();
+				break;
+			case 10:
+				Func.filedirectoryrename();
+				break;
+			case 11:
+				Func.wordcount();
+				break;
+			case 12:
+				Func.movefile();
+				break;
 			default:
 				System.out.println("Error: Undefined case");
 				break;
-			}
+		}
+		input.close();
     }
 }
